d268fda0351b97bebbb84626278f76af641942d0
Jhosevic Castañeda <zetsubou97@gmail.com>
2017-07-26 21:02:08 -0400

    Update socket.py

--- libs/socket.py
+++ libs/socket.py
@@ -1,21 +1,5 @@
 import requests
 from socketIO_client import SocketIO
-#socket = SocketIO('https://lax1.cytu.be:10443')
-
-#Scheduler things
-
-
-#datetime.datetime(year,month,day,hour,minute,second)
-
-
-#join to selected channel
-#socket.emit('joinChannel',{'name':'le_room'})
-#login
-#socket.emit('login',{'name':'le_username','pw':'le_pw'})
-#Add to queue
-#socket.emit("queue",{"le":"dictionary"})
-
-#json = {"id": id, "type": type, "pos": pos, "duration": 0, "temp": temp}
 
 def create_socket(room,user,pw):
 	protocol = room.split(':')[0]
