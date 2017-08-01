aaa3e2370ba4fc2d78b31ad86ab25904f6afec9d
Jhosevic <zetsubo97@gmail.com>
2017-08-01 08:27:06 -0400

    update all

---
+++ libs/utils.py
@@ -0,0 +1,41 @@
+from sanic import response
+from passlib.hash import pbkdf2_sha256 as pbk
+import socket
+import sqlite3
+
+
+LOGIN_ROUTE = "/login"
+conn = sqlite3.connect('../info.db')
+c = conn.cursor()
+
+#Decorator to force login
+def login_required(f):
+	def is_logged(x):
+		return f(x) if x['session'] else response.redirect(LOGIN_ROUTE)
+	return is_logged
+
+
+
+def check_user(user,password):
+	c.execute('SELECT password FROM users WHERE username=?',(user,))
+	result = c.fetchall()
+	if not result:
+		return False
+	return pbk.verify(password,result[0][0])
+	
+def load_sockects():
+	c.execute('SELECT * from user_sockets')
+	users = {}
+	#(owner,user,password,channel)
+	for line in c.fetchall():
+		if line[0] not in users:
+			users[line[0]] = []
+		socket = socket.create_socket(room=line[3],user=line[1],pw=line[2])
+		users[line[0]].append(socket)
+	return users
+
+	
+#not allowing registers
+#def register_user(user,password):
+	#Check if user in db
+	#if not, register
--- server.py
+++ server.py
@@ -1,17 +1,22 @@
 from sanic import Sanic
-from sanic.response import text,json,file,html #response
-from libs import socket
+from sanic.response import text,json,file,html,redirect #response
+from sanic_session import InMemorySessionInterface #session management
+from libs import socket, utils
 from apscheduler.schedulers.background import BackgroundScheduler # scheduler things
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import json
 from datetime import datetime
 
 
+#Manage sessions
+session_interface = InMemorySessionInterface()
+
 #Sanic app, gotta go fast
 app = Sanic('Gotta go fast')
 CONFIG = json.load(open('config','r'))
 ACTIVE = json.load(open('active_jobs','r'))
-socket = socket.create_socket(room=CONFIG['channel'],user=CONFIG['user'],pw=CONFIG['password'])
+#socket = socket.create_socket(room=CONFIG['channel'],user=CONFIG['user'],pw=CONFIG['password'])
+SOCKETS = utils.load_sockets()
 
 #scheduler things
 DB_URL = 'sqlite:///info.db'
@@ -32,12 +37,49 @@ def add_video(url):
 	socket.emit("queue",config)
 	clear_done()
 
-@app.route("/")
+@app.middleware('request')
+async def add_session_to_request(request):
+	# before each request initialize a session
+	# using the client's request
+	await session_interface.open(request)
+
+
+@app.middleware('response')
+async def save_session(request, response):
+	# after each request save the session,
+	# pass the response to set client cookies
+	await session_interface.save(request, response)
+
+
+@app.route("/test")
 def test(request):
-	#return html(open("public/views/index.html",'r').read())
+	for key in request['session']:
+		del request['session'][key]
+	return text("nothing")
+
+@app.route("/")
+def main_page(request):
 	return file('public/views/index.html')
 
-@app.route("/schedule", methods=["POST"])
+@app.route("/login")
+def login(request):
+	if request.method == "GET":
+		if not request['session']:
+			return file('public/views/login.html')
+		return redirect("/user")
+	else:
+		username = request.form.get('username')
+		pw = request.form.get('password')
+		#check user
+		if not utils.check_user(username,pw):
+			return redirect('/login')
+		if not request['session'].get(username):
+			request['session'][username] = 0
+		request['session'][username] += 1
+		return redirect('/user')
+
+@app.route("/schedule")
+@utils.login_required
 def handle_form(request):
 	#Date/time things
 	print(request.form.get('datetime'))
@@ -62,4 +104,4 @@ def handle_form(request):
 
 
 if __name__ == '__main__':
-	app.run(host="localhost",port=8000)
+	app.run(host="localhost",port=8000,debug=True)
